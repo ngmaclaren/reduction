fedf5a255a44c2a760040a837ba0a52e8d781610
ngmaclaren <neil.g.maclaren@gmail.com>
2024-02-28 18:07:46 -0500

    Move development of localsolver to its own git

--- localsolver/R/constants.R
+++
@@ -1,4 +0,0 @@
-#' Number of samples of the control parameter
-#'
-#' Called 'L' in the manuscript, specifies the number of evenly spaced samples from the control (bifurcation) parameter to be used. 
-lout <- 100
