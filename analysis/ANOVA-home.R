d4352a1704b44096d9b67efd31fc6c04cb4f19ec
ngmaclaren <neil.g.maclaren@gmail.com>
2024-03-20 18:47:48 -0400

    Corrected/streamlined ANOVA-{home/other}
    Corrections: forbid A=B for ANOVA-other, always use the double-well
    rand/heur node sets.
    Streamlining: No need to do weights for these files, always use all

--- analysis/ANOVA-home.R
+++ analysis/ANOVA-home.R
@@ -1,152 +1,67 @@
-useall <- "yes" # "no"
-useweights <- "no" # "yes"
-
 networks <- c(
-    "dolphin", "celegans", "proximity", "euroroad", "email", "er", "gkk", "ba", "hk", "lfr"
+                                        # Exclude ER
+    "dolphin", "celegans", "proximity", "euroroad", "email", "gkk", "ba", "hk", "lfr" # , "er"
 )
+                                        # Use all dynamics
 dynamics <- c("doublewell", "SIS", "mutualistic", "genereg")
 ns.types <- c("opt", "fixed", "rand", "constr", "quant", "knnconstr", "comm")
-ns.types <- ns.types[switch(useall, no = 1:3, yes = 1:length(ns.types))]
 
-conds <- expand.grid(networks, dynamics, ns.types, stringsAsFactors = FALSE)
-colnames(conds) <- c("networks", "dynamics", "ns.type")
+names.ns.all <- apply(expand.grid(networks, dynamics), 1, function(row) paste(row, collapse = "_"))
 
-nslistnames <- apply(conds[!duplicated(conds[, 1:2]), 1:2], 1, paste, collapse = "_")
-
+                                        # Need all full states to recompute errors
 fullstates <- lapply(networks, function(network) readRDS(paste0("../data/fullstate-", network, ".rds")))
 names(fullstates) <- networks
-nodesets <- lapply(nslistnames, function(cond) {
-    readRDS(paste0("../data/ns-", cond, switch(useweights, no = ".rds", yes = "_w.rds")))
-}) # _w if useweights
-names(nodesets) <- nslistnames
 
-collect_errors <- function(row, collection) { # this will be apply()ed to the `conds` df
-    ## output: data.frame(error = error, network = network, dynamics = dynamics, ns.type = ns.type)
+nodesets <- lapply(names.ns.all, function(nsname) readRDS(paste0("../data/ns-", nsname, ".rds")))
+names(nodesets) <- names.ns.all
+
+conds <- expand.grid(networks, dynamics, ns.types, stringsAsFactors = FALSE)
+colnames(conds) <- c("network", "dynamics", "ns.type")
+
+collect_errors <- function(row, nodesets, fullstates) {
     network <- row[1]
     dynamic <- row[2]
     ns.type <- row[3]
-    nslist <- paste(c(network, dynamic), collapse = "_")
-    
+
+                                        # this if else statement should output error
+    if(ns.type %in% c("opt", "fixed")) {
+        nsname <- paste(c(network, dynamic), collapse = "_")
+        error <- optNS::get_error(nodesets[[nsname]][[ns.type]])
+    } else if(ns.type %in% c("rand", "constr", "quant", "knnconstr", "comm")) {
+                                        # For heuristic algs and completely random, use the doubewell node sets
+                                        # This is arbitrary: node sets exist for each condition
+        nsname <- paste(c(network, "doublewell"), collapse = "_")
+                                        # But the error will need to be recomputed
+        Y <- fullstates[[network]][[dynamic]]
+        y <- rowMeans(Y)
+
+        ns <- nodesets[[nsname]][[ns.type]]
+
+        error <- sapply(ns, function(x) optNS::obj_fn(x$vs, y = y, Y = Y, optimize_weights = FALSE))
+    }
+
     data.frame(
-        error = optNS::get_error(collection[[nslist]][[ns.type]]),
-        network = network,
-        dynamics = dynamic,
-        ns.type = ns.type,
+        error = error, network = network, dynamics = dynamic, ns.type = ns.type,
         row.names = NULL
     )
 }
 
-df <- do.call(rbind, apply(conds, 1, collect_errors, collection = nodesets))
+df <- do.call(
+    rbind,
+    apply(conds, 1, function(row) collect_errors(as.character(row), nodesets, fullstates))
+)
+
 df$network <- factor(df$network)
 df$dynamics <- factor(df$dynamics)
 df$ns.type <- factor(df$ns.type)
 df$ns.type <- relevel(df$ns.type, "rand")
 
-model.glm <- glm(
-    log(error) ~ network + dynamics + ns.type,
-    data = subset(df, network != "er"),
-    family = gaussian
-)
-summary(model.glm)
+model.glm <- glm(log(error) ~ network + dynamics + ns.type, data = df, family = gaussian)
+model.aov <- aov(log(error) ~ network + dynamics + ns.type, data = df)
 
-## model.lm <- lm(
-##     log(error) ~ network + dynamics + ns.type,
-##     data = subset(df, network != "er")
-## )
-## anova(model.lm)
-
-model.aov <- aov(log(error) ~ network + dynamics + ns.type, data = subset(df, network != "er"))
 anova(model.aov)
+summary(model.glm)
 TukeyHSD(model.aov, "ns.type")
-
-## McFadden's pseudo-R2
 1 - (model.glm$deviance/model.glm$null.deviance)
-
-
-## Below here is code to help interpret the coefficients of this model, and investigate alternatives.
-## The simplest interpretation, though, is as follows:
-## The coefficients on the ns.type levels express the average (across networks and dynamics) difference---on the log scale---between random node sets and the focal node set type. Improvement with optimization is dramatic:
-1/exp(coef(model.glm)["ns.typeopt"])
-                                        # 283.4513
-                                        # 220.2669 # but we're optimizing node weights for random as well. Is that right?
-## That's how many times smaller optimized node set approximation error is, on average.
-1/exp(coef(model.glm)["ns.typefixed"])
-
-
-
-
-
-
-##                                         # avg for opt
-## b.opt <- exp(coefficients(model.glm)["(Intercept)"] + coefficients(model.glm)["ns.typeopt"])
-##                                         # avg for degree-preserving
-## b.fixed <- exp(coefficients(model.glm)["(Intercept)"] + coefficients(model.glm)["ns.typefixed"])
-##                                         # avg for random
-## b.rand <- exp(coefficients(model.glm)["(Intercept)"])
-
-## 1 - (b.rand - b.fixed)/b.rand
-## 1 - (b.rand - b.opt)/b.rand
-
-## pdiff <- function(m) {
-##     x <- coefficients(m)
-##     b <- x
-##     b[-1] <- x[1] + x[-1]
-##     b <- exp(b)
-##     ((b[-1]/b[1])*100) - 100
-## }
-
-## ## sapply(modellist, pdiff)
-## as.data.frame(pdiff(model.glm))
-
-
-## b <- coef(model.glm)
-## exp(b["ns.typeopt"])
-## 1/exp(b["ns.typeopt"]) # the mean optimized error is this many times smaller
-## ## Verify?
-## sapply(df[, 2:4], levels)
-## test <- subset(df, network == "ba" & dynamics == "doublewell")
-## exp(b["ns.typeopt"])*exp(mean(log(test$error[test$ns.type == "rand"])))
-## mean(test$error[test$ns.type == "opt"])
-
-## lapply(split(test, test$ns.type), function(x) summary(log(x$error)))
-## 6.838-4.562
-
-## summary(
-##     glm(log(error) ~ ns.type, data = subset(df, network == "ba" & dynamics == "doublewell"), family = gaussian)
-## )
-
-## ## Ok, these agree, actually. The model coefficient of -5.647 must be the /average/ difference, on the log scale, between random and optimized node sets. Not all conditions achieve that difference, but we expect it to hold on average across the various contrasts.
-
-## ## let's try a couple more
-## lapply(networks[-6], function(net) {
-##     summary(
-##         glm(log(error) ~ ns.type,
-##             data = subset(df, network == net & dynamics == "genereg"),
-##             family = gaussian)
-##     )
-## })
-
-## ## not sure what to do about this:
-## summary(update(model.glm, . ~ . + dynamics:ns.type))
-## ## seems to show that the actual error we achieve depends on the dynamics, but the overall pattern of results doesn't change.
-## mean(c(-2.38, -2.38 + c(-6.149, -1.808, -5.098)))
-##                                         # [1] -5.64375
-## b["ns.typeopt"]
-##                                         # -5.64704 # very close
-## interactionmodel <- update(model.glm, . ~ . + dynamics:ns.type)
-## sapply(list(model.glm, interactionmodel), function(m) 1 - (m$deviance/m$null.deviance))
-##                                         # [1] 0.7012492 0.7955257
-##                                         # explain 9.4% more variance by including the interaction term
-## ## AIC(model.glm, interactionmodel)
-## ##                  df      AIC
-## ## model.glm        19 95466.31
-## ## interactionmodel 37 85947.30
-
-## ## Strangely, optimizing on the double-well model has the least improvement of any of the dynamics. Optimizing on genereg leads to dramatic improvement, for example. You can see these effects by looking at SI figs. S1--S3.
-
-## ## Just because I'm curious...
-## doubleinteraction <- update(interactionmodel, . ~ . + network:ns.type)
-## ## relatively less improvement
-## AIC(model.glm, interactionmodel, doubleinteraction)
-## sapply(list(model.glm, interactionmodel, doubleinteraction), function(m) 1 - (m$deviance/m$null.deviance))
-## ## So, knowing the dynamics is important. That definitely helps us know how much improvement we get by optimizing. However, the basic conclusion---that we get tremendous improvement by optimizing compared to any of the heuristic algorithms---is unaffected by the interaction. 
+exp(coef(model.glm))
+1/exp(coef(model.glm))
--- analysis/ANOVA-other.R
+++ analysis/ANOVA-other.R
@@ -1,101 +1,81 @@
-library(parallel)
-ncores <- detectCores()-1
-
-useall <- "yes" # "no"
-useweights <- "no" # "yes"
-weightsflag <- switch(useweights, no = FALSE, yes = TRUE)
-
+                                        # Opens the same way as ANOVA-home, but the conditions will be different
 networks <- c(
-    "dolphin", "celegans", "proximity", "euroroad", "email", "er", "gkk", "ba", "hk", "lfr"
+                                        # Exclude ER
+    "dolphin", "celegans", "proximity", "euroroad", "email", "gkk", "ba", "hk", "lfr" # , "er"
 )
+                                        # Use all dynamics
 dynamics <- c("doublewell", "SIS", "mutualistic", "genereg")
 ns.types <- c("opt", "fixed", "rand", "constr", "quant", "knnconstr", "comm")
-ns.types <- ns.types[switch(useall, no = 1:3, yes = 1:length(ns.types))]
+                                        # The different ns.types need different handling
+                                        # These two we need for each dynamics
+names.ns.opt <- c("opt", "fixed")
+                                        # These we'll just use the [network]_doublewell node sets
+names.ns.heur <- c("rand", "constr", "quant", "knnconstr", "comm")
 
+names.ns.all <- apply(expand.grid(networks, dynamics), 1, function(row) paste(row, collapse = "_"))
+
+                                        # Need all full states to recompute errors
+fullstates <- lapply(networks, function(network) readRDS(paste0("../data/fullstate-", network, ".rds")))
+names(fullstates) <- networks
+
+nodesets <- lapply(names.ns.all, function(nsname) readRDS(paste0("../data/ns-", nsname, ".rds")))
+names(nodesets) <- names.ns.all
+
+                                        # Different from here
 conds <- expand.grid(networks, dynamics, dynamics, ns.types, stringsAsFactors = FALSE)
-colnames(conds) <- c("networks", "dynamicsA", "dynamicsB", "ns.type")
+colnames(conds) <- c("network", "dynamicsA", "dynamicsB", "ns.type")
+                                        # exclude cases in which A=B
 dups <- apply(conds, 1, function(row) any(duplicated(row)))
 conds <- conds[!dups, ]
 
-## nslistnames <- apply(conds[!duplicated(conds[, 1:2]), 1:2], 1, paste, collapse = "_")
-## nslistnames <- unique(apply(conds[, c("networks", "dynamicsA")], 1, paste, collapse = "_"))
-nslistnames <- apply(expand.grid(networks, dynamics), 1, paste, collapse = "_")
-
-fullstates <- lapply(networks, function(network) readRDS(paste0("../data/fullstate-", network, ".rds")))
-names(fullstates) <- networks
-nodesets <- lapply(nslistnames, function(cond) {
-    readRDS(paste0("../data/ns-", cond, switch(useweights, no = ".rds", yes = "_w.rds")))
-}) # _w if useweights
-names(nodesets) <- nslistnames
-
-collect_errors <- function(row, collection) {
+collect_errors <- function(row, nodesets, fullstates) {
     network <- row[1]
-    dynamicsA <- row[2]
-    dynamicsB <- row[3]
+    dynamicA <- row[2]
+    dynamicB <- row[3]
     ns.type <- row[4]
-    nslist <- paste(c(network, dynamicsA), collapse = "_")
 
-    Y <- fullstates[[network]][[dynamicsB]]
+                                        # error will be handled the same for all, so just need the right node set
+    if(ns.type %in% c("opt", "fixed")) {
+                                        # use the node set for dynamics A
+        nsname <- paste(c(network, dynamicA), collapse = "_")
+    } else if(ns.type %in% c("rand", "constr", "quant", "knnconstr", "comm")) {
+                                        # for heur/rand, use the doublewell node sets
+                                        # Dynamics doesn't matter for these, so use the reference/standard
+        nsname <- paste(c(network, "doublewell"), collapse = "_")
+    }
+
+                                        # Evaluate error against dynamics B (should be no case where A=B)
+    Y <- fullstates[[network]][[dynamicB]]
     y <- rowMeans(Y)
 
-    error <- sapply(
-        collection[[nslist]][[ns.type]],
-        function(x) optNS::obj_fn(x$vs, y = y, Y = Y,
-                                  optimize_weights = switch(ns.type, FALSE, opt = weightsflag),
-                                  ws = x$ws)
-    )
+    ns <- nodesets[[nsname]][[ns.type]]
+
+    error <- sapply(ns, function(x) optNS::obj_fn(x$vs, y = y, Y = Y, optimize_weights = FALSE))
 
     data.frame(
-        error = error, network = network, dynamicsA = dynamicsA, dynamicsB = dynamicsB, ns.type = ns.type,
+        error = error, network = network, dynamicsA = dynamicA, dynamicsB = dynamicB, ns.type = ns.type,
         row.names = NULL
     )
 }
 
-##df <- do.call(rbind, apply(conds, 1, collect_errors, collection = nodesets))
 df <- do.call(
     rbind,
-    mclapply(
-        split(conds, seq(nrow(conds))),
-        function(row) collect_errors(as.character(row), nodesets),
-        ##collect_errors, collection = nodesets,
-        mc.cores = ncores
-    )
+    apply(conds, 1, function(row) collect_errors(as.character(row), nodesets, fullstates))
 )
+
 df$network <- factor(df$network)
 df$dynamicsA <- factor(df$dynamicsA)
 df$dynamicsB <- factor(df$dynamicsB)
 df$ns.type <- factor(df$ns.type)
 df$ns.type <- relevel(df$ns.type, "rand")
 
-dups <- apply(df, 1, function(row) any(duplicated(row)))
-df <- df[!dups, ]
+model.glm <- glm(log(error) ~ network + dynamicsA + dynamicsB + ns.type, data = df, family = gaussian)
+model.aov <- aov(log(error) ~ network + dynamicsA + dynamicsB + ns.type, data = df)
 
-model.glm <- glm(
-    log(error) ~ network + dynamicsA + dynamicsB + ns.type,
-    data = subset(df, network != "er"),
-    family = gaussian
-)
-summary(model.glm)
-
-model.aov <- aov(log(error) ~ network + dynamicsA + dynamicsB + ns.type, data = subset(df, network != "er"))
 anova(model.aov)
+summary(model.glm)
 TukeyHSD(model.aov, "ns.type")
-
-## McFadden's pseudo-R2
 1 - (model.glm$deviance/model.glm$null.deviance)
+exp(coef(model.glm))
+1/exp(coef(model.glm))
 
-## pdiff <- function(m) {
-##     x <- coefficients(m)
-##     b <- x
-##     b[-1] <- x[1] + x[-1]
-##     b <- exp(b)
-##     ((b[-1]/b[1])*100) - 100
-## }
-
-## ## sapply(modellist, pdiff)
-## as.data.frame(pdiff(model.glm))
-1/exp(coef(model.glm)["ns.typeopt"])
-                                        # 14.8734 -- much more sane.
-                                        # 7.094317 with weights. Wow: no reason at all to optimize weights.
-                                        # Never mind: 38.39367 if I don't optimize the node weights of the random
-                                        # node sets. Crazy. 
