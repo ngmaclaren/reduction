4197ad7afe4b1717d130302d241af0ced2172411
ngmaclaren <neil.g.maclaren@gmail.com>
2024-04-12 09:37:47 -0400

    Update KLD and error figures

--- analysis/plot-error-fig.R
+++ analysis/plot-error-fig.R
@@ -10,7 +10,7 @@ wd <- 10
 labelsize <- 1.75
 
 if(save_plots) {
-    pdf("../img/kld-fig.pdf", height = ht, width = wd)
+    pdf("../img/error-fig.pdf", height = ht, width = wd)
 } else {
     dev.new(height = ht, width = wd)
 }
@@ -22,21 +22,22 @@ optpch <- 16 # 1
 randcol <- "#33d17a"
 randpch <- 15 # 0
 
+
 with(dolphin.env, {
-    ylim <- range(c(as.numeric(KLs.rands), KLs.opts))
+    ylim <- range(c(as.numeric(errors.rands), as.numeric(errors.opts)))
     plot(NULL, xlim = xlim, ylim = ylim, xlab = "", ylab = "", xaxt = "n", yaxt = "n", log = "y")
     eaxis(1, cex.axis = labelsize)
     eaxis(2, n.axp = 1, cex.axis = labelsize)
     title(xlab = expression(italic(n)),#"n",
           cex.lab = labelsize)
-    title(ylab = "Kullback-Leibler divergence", line = 4, cex.lab = labelsize)
-    mtext("(a)", line = 0.2, adj = -0.1, cex = labelsize)
-    points(seq(12), KLs.opts, col = optcol, pch = optpch, cex = 3)
-    points(seq(12), colMeans(KLs.rands), col = randcol, pch = randpch, cex = 3)
+    title(ylab = "Approximation error", line = 4, cex.lab = labelsize)
+    mtext("(b)", line = 0.2, adj = -0.1, cex = labelsize)
+    points(seq(12), colMeans(errors.opts), col = optcol, pch = optpch, cex = 3)
+    points(seq(12), colMeans(errors.rands), col = randcol, pch = randpch, cex = 3)
     segments(
         x0 = seq(12),
-        y0 = apply(KLs.rands, 2, CI, -1.96),
-        y1 = apply(KLs.rands, 2, CI, 1.96),
+        y0 = apply(errors.rands, 2, CI, -1.96),
+        y1 = apply(errors.rands, 2, CI, 1.96),
         col = randcol, lwd = 2
     )
     legend(
@@ -46,26 +47,23 @@ with(dolphin.env, {
 })
 
 with(ba.env, {
-    ylim <- range(c(as.numeric(KLs.rands), KLs.opts))
+    ylim <- range(c(as.numeric(errors.rands), as.numeric(errors.opts)))
     plot(NULL, xlim = xlim, ylim = ylim, xlab = "", ylab = "", xaxt = "n", yaxt = "n", log = "y")
     eaxis(1, cex.axis = labelsize)
     eaxis(2, n.axp = 1, cex.axis = labelsize)
     title(xlab = expression(italic(n)),#"n",
           cex.lab = labelsize)
-    title(ylab = "Kullback-Leibler divergence", line = 4, cex.lab = labelsize)
+    title(ylab = "Approximation error", line = 4, cex.lab = labelsize)
     mtext("(b)", line = 0.2, adj = -0.1, cex = labelsize)
-    points(seq(12), KLs.opts, col = optcol, pch = optpch, cex = 3)
-    points(seq(12), colMeans(KLs.rands), col = randcol, pch = randpch, cex = 3)
+    points(seq(12), colMeans(errors.opts), col = optcol, pch = optpch, cex = 3)
+    points(seq(12), colMeans(errors.rands), col = randcol, pch = randpch, cex = 3)
     segments(
         x0 = seq(12),
-        y0 = apply(KLs.rands, 2, CI, -1.96),
-        y1 = apply(KLs.rands, 2, CI, 1.96),
+        y0 = apply(errors.rands, 2, CI, -1.96),
+        y1 = apply(errors.rands, 2, CI, 1.96),
         col = randcol, lwd = 2
     )
-    ## legend(
-    ##     "topright", bty = "n", col = c(optcol, randcol), pch = c(optpch, randpch), pt.cex = 2, cex = 0.75*labelsize,
-    ##     legend = c("Optimized", "Random")
-    ## )
 })
-       
+
+
 if(save_plots) dev.off()
