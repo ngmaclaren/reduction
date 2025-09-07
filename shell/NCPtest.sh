3d99bb295398361af1626f4c1fcdd5b6047c52a2
Neil MacLaren <45513933+ngmaclaren@users.noreply.github.com>
2025-09-07 06:36:07 -0400

    Delete shell directory

--- shell/NCPtest.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-NCPs=(20 30 40 50 60 70 80 90 100)
-
-for NCP in ${NCPs[@]}; do
-    time Rscript ../sims/simulate-full-system.R --network=BAtest150 --ncparam=$NCP
-done
--- shell/make-all-full-systems-weighted.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-networks=(windsurfers macaques train_terrorists highschool drug residence_hall netsci_weighted proximity_weighted gap_junction_herm intl_trade)
-
-for network in ${networks[@]}; do
-    jobname=fs-$network
-    # echo $jobname
-    sbatch --job-name=$jobname --output=./output/${jobname}.out request-full-system.sh $network
-done
